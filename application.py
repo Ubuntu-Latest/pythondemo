5a7907427d2667f386858ced9f9d8b62cc041b42
Vaishnav Nugala <vaishnav.nugala@ecanarys.com>
2021-12-21 23:03:27 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! from actions"
 
 # run the app.
 if __name__ == "__main__":
